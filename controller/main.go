68a5d285032e1f74d5e130537d78fd97124c6f5f
Etienne Journet <etienne.journet@gmail.com>
2021-06-05 09:39:29 +0200

    Added manifests for deployment and some bug resolve in main.go

--- controller/main.go
+++ controller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+  "os"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,14 +14,24 @@ func main() {
 
 	cert, key := wh.createCert()
 
-	ioutil.WriteFile("cert.key", key, 0644)
-	ioutil.WriteFile("cert.pem", cert, 0644)
+  userHomeDir, err := os.UserHomeDir()
+  if err != nil {
+    log.Fatal(err)
+  }
+	err = ioutil.WriteFile(userHomeDir+"/cert.key", key, 0644)
+  if err != nil {
+    log.Fatal(err)
+  }
+	err = ioutil.WriteFile(userHomeDir+"/cert.pem", cert, 0644)
+  if err != nil {
+    log.Fatal(err)
+  }
 	wh.alterMutatingWebhook(cert)
 
 	http.HandleFunc("/", wh.server)
 
 	log.Print("Listening on port ", wh.Port)
-	err := http.ListenAndServeTLS(":"+strconv.Itoa(wh.Port), "cert.pem", "cert.key", nil)
+	err = http.ListenAndServeTLS(":"+strconv.Itoa(wh.Port), userHomeDir+"/cert.pem", userHomeDir+"/cert.key", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
