2a91562aa2cf62788241e2a3260cb2a5580f6d9e
Etienne Journet <etienne.journet@gmail.com>
2021-07-15 16:30:18 +0200

    test

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh - --write-kubeconfig-mode 644
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-whoami
-ls -la /etc/rancher/k3s/k3s.yaml
 
 k3s kubectl create ns super-simple-mesh
 k3s kubectl apply -f deploy/manifest
