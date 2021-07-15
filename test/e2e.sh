f1436ef5511f138076edfa9297094338f9ffdc43
Etienne Journet <etienne.journet@gmail.com>
2021-07-15 16:13:41 +0200

    test

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-export KUBECONFIG="kubeconfig"
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 
 curl -sfL https://get.k3s.io | sh -
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
-mv kubectl /usr/local/bin
 
-kubectl create ns super-simple-mesh
-kubectl apply -f deploy/manifest
+k3s kubectl create ns super-simple-mesh
+k3s kubectl apply -f deploy/manifest
 helm install cert-manager jetstack/cert-manager --set installCRDs=true -n super-simple-mesh
 
 go test -v
--- test/mesh_test.go
+++ test/mesh_test.go
@@ -17,7 +17,7 @@ import (
 // An example of how to test the Kubernetes resource config in examples/kubernetes-basic-example using Terratest.
 func TestKubernetesBasicExample(t *testing.T) {
 	ssmNamespace := "super-simple-mesh"
-	options := k8s.NewKubectlOptions("", "kubeconfig", ssmNamespace)
+	options := k8s.NewKubectlOptions("", "/etc/rancher/k3s/k3s.yaml", ssmNamespace)
 	//k8s.CreateNamespace(t, options, ssmNamespace)
 
 	k8s.KubectlApply(t, options, "./manifest/clusterissuer.yml")
