29d3b3e6fe722d722224417ba118e26b8875105c
Etienne Journet <etienne.journet@gmail.com>
2021-07-15 17:10:07 +0200

    test

--- test/e2e.sh
+++ test/e2e.sh
@@ -7,13 +7,13 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 ## Create Namespace
 kubectl create ns super-simple-mesh
 
-## Setup SSM
-kubectl apply -f ../deploy/manifest
-
 ## Setup Cert-Manager using Helm
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager --set installCRDs=true -n super-simple-mesh
+
+## Setup SSM
+kubectl apply -f ../deploy/manifest
 
 ## Setup ClusterIssuer
 kubectl apply -f ./manifest/clusterissuer.yml
