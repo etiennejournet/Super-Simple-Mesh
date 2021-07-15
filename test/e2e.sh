c98f5663bd7e7ef6e50f357d86e9df9224549f96
Etienne Journet <etienne.journet@gmail.com>
2021-07-15 16:40:36 +0200

    test

--- test/e2e.sh
+++ test/e2e.sh
@@ -4,8 +4,15 @@ export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s -
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
+## Create Namespace
 kubectl create ns super-simple-mesh
-kubectl apply -f deploy/manifest
+
+## Setup SSM
+kubectl apply -f ../deploy/manifest
+
+## Setup Cert-Manager using Helm
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
 helm install cert-manager jetstack/cert-manager --set installCRDs=true -n super-simple-mesh
 
 go test -v
